3b2bea5587fe9f6db9270de888d59589727d8c3c
Zeron Official <94586700+ZeronoC@users.noreply.github.com>
2022-08-26 20:57:41 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -148,10 +148,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
-    conn.sendHydrated(m.chat, text.trim(), author, thumbmenu, sig, 'Instagram', `+${global.ownernumber}`, 'Number Owner', [
-      ['𝐃𝐨𝐧𝐚𝐭𝐞', '/donasi'],
-      ['𝐒𝐩𝐞𝐞𝐝', '/ping'],
-      ['𝐂𝐫𝐞𝐚𝐭𝐨𝐫', '/owner']
+    conn.sendHydrated(m.chat, }`, [
+      ['Donasi', '/donasi'],
+      ['Tes Bot', '/ping'],
+      ['Pemilik', '/owner']
     ], m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
