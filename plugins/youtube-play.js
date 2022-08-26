048a12b3b436c400bf709755cb874e6dc8c50d00
Zeron Official <94586700+ZeronoC@users.noreply.github.com>
2022-08-26 22:15:52 +0700

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -5,18 +5,15 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!vid) throw 'Video/Audio Tidak ditemukan'
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
-  await conn.sendButton(m.chat,
-'📌 *Title:* ${title}
+  await conn.sendButton(m.chat, '
+📌 *Title:* ${title}
 🔗 *Url:* ${url}
 🖹 *Description:* ${description}
 ⏲️ *Published:* ${publishedTime}
 ⌚ *Duration:* ${durationH}
 👁️ *Views:* ${viewH}
-*JANGAN DISPAMM*',
-  [
-    ['Audio 🎧', `${usedPrefix}yta ${url} yes`], 
-    ['Video 🎥', `${usedPrefix}ytv ${url} yes`]
-  ], m)
+*JANGAN DISPAMM*', author, thumbnail, url, '📺Go To Youtube!', null, null, [['Audio 🎧', `${usedPrefix}yta ${url} yes`], 
+    ['Video 🎥', `${usedPrefix}ytv ${url} yes`]], m)
 }
 handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
